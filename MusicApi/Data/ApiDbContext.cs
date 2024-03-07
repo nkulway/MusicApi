0ed3b69c72f188880365fd4f07f434090dd899ec
“Nick” <nkulway@gmail.com>
2024-03-06 20:37:40 -0500

    sql server

---
+++ MusicApi/Data/ApiDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using MusicApi.Models;
+
+namespace MusicApi.Data
+{
+	public class ApiDbContext : DbContext
+	{
+		public ApiDbContext(DbContextOptions<ApiDbContext>options) : base(options)
+		{
+
+		}
+		public DbSet<Song> Songs { get; set; }
+	}
+}
+
--- MusicApi/Program.cs
+++ MusicApi/Program.cs
@@ -1,4 +1,7 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using MusicApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApiDbContext>(option => option.UseSqlServer(@"Data Source=Your Server Name;Initial Catalog=Your Database Name;"));
 
 var app = builder.Build();
 
@@ -16,6 +20,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+//ApiDbContext dbcontext = app.Services.GetRequiredService<ApiDbContext>();
+//dbcontext.Database.EnsureCreated();
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
@@ -23,4 +30,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
